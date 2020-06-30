e0ac045521e6f021e2faac54b5a3bb2b363bd810
danielbarros338 <danielbarros338@gmail.com>
2020-06-30 15:09:22 -0300

    Pasta _img mudada para img | alteração no HTML

---
+++ README.md
@@ -0,0 +1,20 @@
+# MarkUx
+#### Ficticious enterprise website project (Single page application)
+
+
+This website use:
+
+ - HTML
+ - CSS
+ - Javascript
+ - Bootstrap
+ 
+His idea is to be a website for the presentation of a fictitious marketink and technology company.
+It is responsive and by clicking on the "Empresa" and "Visao" anchors, they take you to the place where you have a text
+related.
+
+#### Normal
+![](https://i.imgur.com/6PpQDnF.gif)
+
+#### Responsive
+![](https://i.imgur.com/BIYPURi.gif)
---
+++ animacoes.js
@@ -0,0 +1,67 @@
+//scroll
+const cabec = document.querySelectorAll(".cabecalho");
+window.addEventListener("scroll", function scrollCabecalho(){
+    if(window.scrollY > 50) {
+        cabec[0].style.transition = "0.5s";
+        cabec[0].style.top = "-120px";
+    }else{
+        cabec[0].style.transition = "0.5s";
+        cabec[0].style.top = "0px";
+    }
+})
+
+const banner = document.querySelectorAll(".banner");
+window.addEventListener("scroll", function scrollBanner(){
+    if(window.scrollY > 1) {
+        banner[0].style.transition = "1s";
+        banner[0].style.opacity = "1";
+        banner[0].style.left = "0";
+    }
+    if(window.scrollY > 1850){
+        banner[1].style.transition = "1.5s";
+        banner[1].style.opacity = "1";
+        banner[1].style.left = "0";
+    }
+})
+
+const mux1 = document.querySelectorAll(".markuxfff");
+const mux2 = document.querySelectorAll(".markuxf000");
+window.addEventListener("scroll", function scrollImgs(){
+    if(window.scrollY > 550) {
+        mux1[0].style.transition = "1.5s";
+        mux1[0].style.opacity = "1";
+    }
+    if(window.scrollY > 1380) {
+        mux2[0].style.transition = "1.5s";
+        mux2[0].style.opacity = "1";
+    }
+})
+
+const empresa = document.querySelectorAll(".empresa");
+const visao = document.querySelectorAll(".visao");
+window.addEventListener("scroll", function scrollTxt(){
+    if(window.scrollY >250) {
+        empresa[0].style.transition = "1.5s";
+        empresa[0].style.opacity = "1";
+    }
+    if(window.scrollY > 1080) {
+        visao[0].style.transition = "2s";
+        visao[0].style.left = "0";
+        visao[0].style.opacity = "1";
+    }
+})
+
+const ancora = document.querySelectorAll(".ancora");
+ancora.forEach(item =>{ //"Percorrera" todos os itens
+    item.addEventListener("click", function scrollPraId(event){
+        event.preventDefault(); // Previne o comportamento padrao do href 
+        const element = event.target; // Pega o elemento clicado
+        const id = element.getAttribute('href'); // Pega os atributos href
+        const to = document.querySelector(id).offsetTop; // Pega a posiçao do topo do elemento
+        
+        window.scroll({ //Configura meu scroll para o elemento
+            top: to - 20,
+            behavior: "smooth",
+        })
+    })
+})
